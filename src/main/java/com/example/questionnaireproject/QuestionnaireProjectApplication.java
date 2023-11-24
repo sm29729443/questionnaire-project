63bae39f4f64e7d9df364e877457b6fb3072681f
Fish <sm29729443@gmail.com>
2023-11-24 09:57:01 +0800

    initial spring boot setting

---
+++ src/main/java/com/example/questionnaireproject/QuestionnaireProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionnaireproject/QuestionnaireProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
